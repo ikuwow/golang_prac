be221df28de7ba58b4141e60e05e662d05ffa4c7
Ikuo Degawa <degawa@ikuwow.com>
2015-08-02 01:07:58 +0900

    Understand go variables

--- hello.go
+++ hello.go
@@ -27,5 +27,21 @@ func main() {
 
     // 変数や関数名は、
     // 頭が大文字だと他のパッケージからでも参照できる
-    // 頭が小文字だとこのパッケージの中空参照する
+    // 頭が小文字だとこのパッケージの中から参照する
+
+    /* data types
+    string "hello"
+    int 53 (int8 int16)
+    float64 ( 10.2)
+    bool true, false
+    nil
+    */
+
+    // initial values
+    var s string // "" empty string!!
+    var f bool // false !
+    var i int // 0 ! ¥¥
+
+    fmt.Printf("a: %s, b:%f, c:%d¥n", s, f, i)
+
 }
