08f4e7dfcc7714d8be209b2e7a839d705d41511d
Ikuo Degawa <degawa@ikuwow.com>
2015-08-02 00:59:41 +0900

    Variables handling

--- hello.go
+++ hello.go
@@ -5,7 +5,27 @@ import "fmt"
 // like "stdout"
 
 func main() {
-    fmt.Println("hello world")
+
+    // var message string
+    // message = "hello world"
+    // ↓
+    // var message = "hello world"
+    // ↓
+    message := "hello world" // よく出る
+
+    // when define multiple variables simultaneously
+    // var a,b int
+    // a, b  = 10, 5
+    a, b := 10, 15
+
+    fmt.Println(message)
+
+    fmt.Println(a)
+    fmt.Println(b)
     // no semicolons
     // tab indent (not spaces) are prefered
+
+    // 変数や関数名は、
+    // 頭が大文字だと他のパッケージからでも参照できる
+    // 頭が小文字だとこのパッケージの中空参照する
 }
